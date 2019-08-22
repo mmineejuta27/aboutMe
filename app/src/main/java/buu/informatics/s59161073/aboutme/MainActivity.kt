c155b7470ee140b33b8a85b4e620da577528af6d
mmineejuta27 <minejutamas@gmail.com>
2019-08-23 00:45:36 +0700

    binding 4

--- app/src/main/java/buu/informatics/s59161073/aboutme/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59161073/aboutme/MainActivity.kt
@@ -14,6 +14,7 @@ import buu.informatics.s59161073.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private  lateinit var binding: ActivityMainBinding
+    private val myName:MyName = MyName("Jutamas Soiraya")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
@@ -24,7 +25,9 @@ class MainActivity : AppCompatActivity() {
             nicknameText.setOnClickListener {
                 updateNickname(it)
             }
+            this.myName = this@MainActivity.myName
         }
+
 
     }
 
@@ -43,10 +46,11 @@ class MainActivity : AppCompatActivity() {
     private fun addNickname(view: View){
 
         binding.apply {
-            nicknameText.text = nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
             nicknameEdit.visibility = View.GONE
             nicknameText.visibility = View.VISIBLE
             doneButton.visibility = View.GONE
+            invalidateAll()
             //Hide Keyboard
             val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
---
+++ app/src/main/java/buu/informatics/s59161073/aboutme/MyName.kt
@@ -0,0 +1,3 @@
+package buu.informatics.s59161073.aboutme
+
+data class MyName(var name: String = "", var nickname: String = "")
